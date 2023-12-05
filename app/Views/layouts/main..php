d0b116a6aa0acd01ba88caa94306572bc47d32c8
dafahan <dafahanid@gmail.com>
2023-12-05 17:06:24 +0700

    initial commit

---
+++ app/Views/layouts/main..php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="../img/logo.png" type="image/x-icon"/>
+    <script>
+      WebFont.load({
+        google: {"families":["Lato:300,400,700,900"]},
+        custom: {"families":["Flaticon", "Font Awesome 5 Solid", "Font Awesome 5 Regular", "Font Awesome 5 Brands", "simple-line-icons"], urls: ['../assets/css/fonts.min.css']},
+        active: function() {
+          sessionStorage.fonts = true;
+        }
+      });
+    </script>
+    <title>{{ $title }}</title>
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+  </head>
+
+  <style>
+    body{
+        font-family: 'Nunito', sans-serif;
+    }
+  </style>
+
+  <body>
+   
+    @include('partials.navbar')
+
+    <div>
+        @yield('container')
+    </div>
+    
+    @include('partials.footer')
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+  </body>
+</html>
